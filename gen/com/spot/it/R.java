3c330263a1083d0862ff182a66925261a078eb24
SetiZ <funkysetiz@gmail.com>
2014-04-25 00:00:46 +0200

    made the login part

--- gen/com/spot/it/R.java
+++ gen/com/spot/it/R.java
@@ -995,13 +995,13 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003e;
+        public static final int action_settings=0x7f050040;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
-        public static final int container=0x7f05003c;
+        public static final int container=0x7f05003f;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1017,10 +1017,12 @@ containing a value of this type.
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
+        public static final int logingo=0x7f05003e;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int password=0x7f05003d;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1038,7 +1040,6 @@ containing a value of this type.
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
-        public static final int sign_in_button=0x7f05003d;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
@@ -1046,6 +1047,7 @@ containing a value of this type.
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
+        public static final int username=0x7f05003c;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1097,9 +1099,10 @@ containing a value of this type.
         public static final int abc_popup_menu_item_layout=0x7f030014;
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
-        public static final int activity_main=0x7f030017;
-        public static final int fragment_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_login=0x7f030017;
+        public static final int activity_main=0x7f030018;
+        public static final int fragment_main=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1148,6 +1151,7 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int login_go=0x7f0a0010;
     }
     public static final class style {
         /** 
---
+++ src/com/spot/it/LogInActivity.java
@@ -0,0 +1,62 @@
+package com.spot.it;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LogInActivity extends Activity {
+
+	EditText username;
+	EditText password;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_login);
+
+		username = (EditText) findViewById(R.id.username);
+		password = (EditText) findViewById(R.id.password);
+		Button go = (Button) findViewById(R.id.logingo);
+
+		go.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				ParseUser.logInInBackground(username.getText().toString(),
+						password.getText().toString(), new LogInCallback() {
+
+							@Override
+							public void done(ParseUser user, ParseException e) {
+								if (e != null) {
+									// Show the error message
+									Toast.makeText(LogInActivity.this,
+											e.getMessage(), Toast.LENGTH_LONG)
+											.show();
+								} else {
+									// Start an intent for the dispatch activity
+									Intent intent = new Intent(
+											LogInActivity.this,
+											SpotActivity.class);
+									intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK
+											| Intent.FLAG_ACTIVITY_NEW_TASK);
+									startActivity(intent);
+								}
+
+							}
+						});
+
+			}
+		});
+
+	}
+}
