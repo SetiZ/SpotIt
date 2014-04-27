d528b10a27223435c00510558a14b17a44c2a032
SetiZ <funkysetiz@gmail.com>
2014-04-27 12:49:07 +0200

    clean up code

--- src/com/spot/it/Application.java
+++ src/com/spot/it/Application.java
@@ -1,41 +1,17 @@
 package com.spot.it;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-
 import com.parse.Parse;
 
 public class Application extends android.app.Application {
-  // Debugging switch
-  public static final boolean APPDEBUG = false;
-  
-  // Debugging tag for the application
-  public static final String APPTAG = "SpotIt";
 
-  // Key for saving the search distance preference
-  private static final String KEY_SEARCH_DISTANCE = "searchDistance";
+	public Application() {
+	}
 
-  private static SharedPreferences preferences;
-
-  public Application() {
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-
-    //ParseObject.registerSubclass(AnywallPost.class);
-    Parse.initialize(this, "wlmt8F0Q1NPM3JPninKpUVKzEjXpLaw4OyThbVQc",
-        "r7DsHein7vkiq0oRx5JAfUZ6rlA6vJ8wCl5JFhek");
-    preferences = getSharedPreferences("com.parse.anywall", Context.MODE_PRIVATE);
-  }
-
-  public static float getSearchDistance() {
-    return preferences.getFloat(KEY_SEARCH_DISTANCE, 250);
-  }
-
-  public static void setSearchDistance(float value) {
-    preferences.edit().putFloat(KEY_SEARCH_DISTANCE, value).commit();
-  }
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Parse.initialize(this, "wlmt8F0Q1NPM3JPninKpUVKzEjXpLaw4OyThbVQc",
+				"r7DsHein7vkiq0oRx5JAfUZ6rlA6vJ8wCl5JFhek");
+	}
 
 }
